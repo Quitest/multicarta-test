6b01232d61db9efb5266ed18045f706b346ab1da
Quitest <Quitest>
2022-08-18 00:17:20 +0300

    Коррекция доступа в разделы сайта. Доступность раздела зависит от роли.

--- src/main/java/ru/pel/tests/multicartatest/config/SecurityConfig.java
+++ src/main/java/ru/pel/tests/multicartatest/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,18 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-
                 .authorizeRequests()
-                .antMatchers("/api/*").hasRole("ADMIN")
-                .antMatchers("/reports/*").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/").permitAll()
+                .antMatchers("/api/**", "/payments/**").hasRole("ADMIN")
+                .antMatchers("/reports/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic()
-                .and()
-                .logout()
-                .deleteCookies("JSESSIONID");
-
+                .httpBasic();
     }
 
     @Bean
